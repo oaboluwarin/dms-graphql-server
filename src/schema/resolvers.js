d353c605f01e3f74adafa6cdabafe20450b50371
oaboluwarin <aboluwarinoreoluwa@gmail.com>
2019-06-30 23:30:35 +0100

    update resolvers

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -27,10 +27,15 @@ const resolvers = {
           'Permission denied. Only Admins can query for all users'
         );
       }
-      return await Users.find({}).exec();
+      return await Users.find({})
+        .populate('documents')
+        .exec();
     },
     getDocuments: async () => {
-      return await Documents.find({}).exec();
+      const docs = await Documents.find({})
+        .populate('owner')
+        .exec();
+      return docs;
     }
   },
   Mutation: {
@@ -70,6 +75,70 @@ const resolvers = {
 
         const token = await createToken(user);
         return { token };
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+    updateUser: async (_, args) => {
+      try {
+        const updatedUser = await Users.findByIdAndUpdate(args.id, args, {
+          new: true
+        });
+        if (!updatedUser) {
+          throw new Error('User could not be updated');
+        }
+        return updatedUser;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+    deleteUser: async (_, args) => {
+      try {
+        const userFound = await Users.findByIdAndDelete(args.id);
+        if (userFound) {
+          return { message: 'User deleted' };
+        }
+        throw new Error('User does not exist');
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+    createDocument: async (parent, args, context) => {
+      try {
+        const { title, content, access = 'PUBLIC' } = args;
+        const newDocument = await Documents.create({
+          title,
+          content,
+          access,
+          owner: context.user.id
+        });
+        return newDocument;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    updateDocument: async (_, args) => {
+      try {
+        const updatedDocument = await Documents.findByIdAndUpdate(
+          args.id,
+          args,
+          { new: true }
+        );
+        if (!updatedDocument) {
+          throw new Error('Document could not be updated');
+        }
+        return updatedDocument;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+    deleteDocument: async (_, args) => {
+      try {
+        const documentFound = await Documents.findByIdAndDelete(args.id);
+        if (documentFound) {
+          return { message: 'Document deleted' };
+        }
+        throw new Error('Document does not exist');
       } catch (error) {
         throw new Error(error.message);
       }
