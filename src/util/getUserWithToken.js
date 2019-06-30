953a125e75801ff682508e20d38b7a312e8fe897
oaboluwarin <aboluwarinoreoluwa@gmail.com>
2019-06-30 23:30:24 +0100

    add error handling condition to getUserWithToken

--- src/util/getUserWithToken.js
+++ src/util/getUserWithToken.js
@@ -2,9 +2,16 @@ import jwt from 'jsonwebtoken';
 import Users from '../schema/user.schema';
 
 const getUserWithToken = async token => {
-  const userFromToken = await jwt.verify(token, process.env.JWT_SECRET);
-  const user = await Users.findById(userFromToken.id);
-  return user;
+  try {
+    if (token) {
+      const userFromToken = await jwt.verify(token, process.env.JWT_SECRET);
+      const user = await Users.findById(userFromToken.id);
+      return user;
+    }
+    throw new Error('No token provided');
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
 export default getUserWithToken;
